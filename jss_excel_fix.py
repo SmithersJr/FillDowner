c0072d13e2d042d568cb0af8e904f34f91759187
Rob Smithers <robsmithers@gmail.com>
2016-01-08 13:51:22 -0500

    Initial commit

---
+++ jss_excel_fix.py
@@ -0,0 +1,41 @@
+import csv
+
+def process_rows(row):
+	global a
+	global b 
+	
+	if row[0] == "":
+		row[0] = a
+	else:
+		a = row[0]
+		
+	if row[1] == "":
+		row[1] = b
+	else:
+		b = row[1]
+		
+	writer.writerow(row)
+	print row
+
+				
+with open('121 Computers in All Macs.csv', 'r') as f, open('results.csv', 'w') as results:
+    reader = csv.reader(f)
+    writer = csv.writer(results)
+
+    for row in reader:
+    	process_rows(row)
+    	
+#    print f
+#     print
+#     print count
+
+# def get_columns():
+# 	empty_columns = list(raw_input("Which columns need to be filled? "))
+# 	empty_columns = map(int, empty_columns)
+# 	print empty_columns
+    
+
+# get_columns()
+
+    # 14696 rows total
+    # 11839 blanks
