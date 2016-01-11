ffe2da6f40937ec9aca1273ec4d0442e04d0248a
Rob Smithers <robsmithers@gmail.com>
2016-01-11 17:03:17 -0500

    Adding dialogs
    Adding dialogs to select original CSV file to work on and results CSV
    file to save.

--- FillDowner.py
+++ FillDowner.py
@@ -1,35 +1,68 @@
+import sys
 import csv
+import Tkinter as tk
 import tkFileDialog
+import tkMessageBox
 
-				
-with open('121 Computers in All Macs.csv', 'r') as f, open('results.csv', 'w') as results:
-    reader = csv.reader(f)
-    writer = csv.writer(results)
+# Dialogs to select the files we're working with
+def get_original_file():
+    original_filename = ""
+    
+    root = tk.Tk()
+    root.withdraw()
+    original_filename = tkFileDialog.askopenfilename(defaultextension = ".csv")
+    if original_filename == "":
+        sys.exit("No file selected")
+    elif original_filename.lower().endswith(".csv"):
+        return original_filename
+    else:
+        tkMessageBox.showerror("Error!", "You must select a CSV file.", icon = "error")
+        get_original_file()
 
-    for row in reader:
-    	process_rows(row)
+def get_results_file():
+	results_filename = ""
 
+	root = tk.Tk()
+	root.withdraw()
+	results_filename = tkFileDialog.asksaveasfilename(defaultextension = ".csv")
+	if results_filename == "":
+		if tkMessageBox.askyesno("Cancel?", "Are you sure you want to cancel processing this file?"):
+			sys.exit("File save cancelled")
+		else:
+			get_results_file()
+	else:
+		return results_filename
+
+# The process to "fill down" the blank fields
 def process_rows(row):
 	global a
 	global b 
-	
+ 	
 	if row[0] == "":
 		row[0] = a
 	else:
 		a = row[0]
-		
+ 		
 	if row[1] == "":
 		row[1] = b
 	else:
 		b = row[1]
-		
+ 		
 	writer.writerow(row)
 	print row
-    	
-#    print f
-#     print
-#     print count
+ 
+ # Do it!
+get_original_file()
+get_results_file()
 
+# # Open the files and process them			
+# with open('121 Computers in All Macs.csv', 'r') as f, open('results.csv', 'w') as results:
+#     reader = csv.reader(f)
+#     writer = csv.writer(results)
+# 
+#     for row in reader:
+#     	process_rows(row)
+    	
 # def get_columns():
 # 	empty_columns = list(raw_input("Which columns need to be filled? "))
 # 	empty_columns = map(int, empty_columns)
